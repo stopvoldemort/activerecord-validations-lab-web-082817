3a12a8ddf342c72e74e6fbd7a0a0b8ad4f7723b1
stopvoldemort <david.squires1@gmail.com>
2017-09-25 11:37:12 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :title_is_click_baity
+
+  def title_is_click_baity
+    if !/Won't believe|Secret|Guess|Top \d/i.match(self.title)
+      errors.add(:title, "not sufficiently click-baity")
+    end
+  end
 end
